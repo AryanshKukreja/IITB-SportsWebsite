f93ffc31f7edee1d6bb8496010d790ed99591819
ShubhSarin <shubhsarin08@gmail.com>
2025-09-06 14:05:42 +0530

    full screen and download button changes

--- client/src/components/yearbook/yearbook.js
+++ client/src/components/yearbook/yearbook.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import "./yearbook.css";
-import { FaRegArrowAltCircleDown } from "react-icons/fa";
 import { PiCornersOut } from "react-icons/pi";
 import { PiCornersIn } from "react-icons/pi";
 
@@ -10,15 +9,6 @@ const Yearbook = () => {
   
   const toggleFullscreen = () => {
     setIsFullscreen(!isFullscreen);
-  };
-
-  const handleDownload = () => {
-    const link = document.createElement('a');
-    link.href = pdfUrl;
-    link.download = 'Sports_Yearbook_2024-25.pdf';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
   };
 
   const containerStyle = isFullscreen ? {
@@ -47,28 +37,6 @@ const Yearbook = () => {
     borderRadius: '4px'
   };
 
-  /* const buttonStyle = {
-    backgroundColor: '#00000000',
-    color: 'white',
-    border: 'none',
-    padding: '10px 15px',
-    borderRadius: '4px',
-    cursor: 'pointer',
-    fontSize: '14px',
-    fontWeight: 'bold',
-    boxShadow: '0 2px 5px rgba(0,0,0,0.2)',
-    marginRight: '10px',
-  }; */
-
-  const buttonContainerStyle = {
-    position: isFullscreen ? 'fixed' : 'absolute',
-    top: isFullscreen ? '12px' : '12px',
-    right: isFullscreen ? '160px' : '160px',
-    zIndex: 1,
-    display: 'flex',
-    gap: '10px',
-  };
-
   return (
     <div style={containerStyle}>
       {!isFullscreen && (
@@ -83,45 +51,23 @@ const Yearbook = () => {
         position: 'relative'
       }}>
         <div className="yb-button-container">
-          {/* <button 
-            onClick={handleDownload}
-            style={buttonStyle}
-            onMouseOver={(e) => e.target.style.backgroundColor = '#00000044'}
-            onMouseOut={(e) => e.target.style.backgroundColor = '#00000000'}
-            title="Download PDF"
-          >
-            
-          </button> */}
-          <div className="download-icon">
-          <FaRegArrowAltCircleDown 
-          onClick={handleDownload} 
-          size={20} 
-          style={{ verticalAlign: 'middle', cursor: "pointer" }}
-          /> </div>
+          
           <div>
 
           {isFullscreen ? <PiCornersIn 
+          className="yb-corner"
           onClick={toggleFullscreen} 
           size={20} 
           style={{ verticalAlign: 'middle', cursor: "pointer" }}
           /> : <PiCornersOut 
+          className="yb-corner"
           onClick={toggleFullscreen} 
           size={20} 
           style={{ verticalAlign: 'middle' , cursor: "pointer"}} 
           />}
           </div>
 
-          {/* <button 
-            onClick={toggleFullscreen}
-            style={buttonStyle}
-            onMouseOver={(e) => e.target.style.backgroundColor = '#00000044'}
-            onMouseOut={(e) => e.target.style.backgroundColor = '#00000000'}
-            title={isFullscreen ? 'Exit Fullscreen' : 'Enter Fullscreen'}
-            fontSize={25}
-            size={40}
-          >
-            
-          </button> */}
+          
         </div>
         
         <iframe
